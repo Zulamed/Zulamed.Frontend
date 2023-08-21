49a73e9b462df4f16120e2bbe216469242eeeabe
rashiddalv <rashiddalv@gmail.com>
2023-08-21 12:26:22 +0400

    create a new password page

---
+++ src/routes/createNewPass/+page.svelte
@@ -0,0 +1,160 @@
+<script lang="ts">
+	import CreateNewPassForm from './components/createNewPassForm/createNewPassForm.svelte';
+</script>
+
+<section class="container forms">
+	<img class="container-img" src="img/main-background-mobile.png" alt="" />
+	<div class="container-overlay" />
+	<div class="form register">
+		<header>
+			<h2>
+				Request a Password <br />
+				Reset
+			</h2>
+		</header>
+		<CreateNewPassForm />
+		<div class="form-link">
+			<a href="/login" class="link signin-link">Click here</a> <span>to go back to Login</span>
+		</div>
+	</div>
+</section>
+
+<style>
+	.note {
+		color: #585858;
+		text-align: center;
+		font-size: 12px;
+		font-style: normal;
+		font-weight: 400;
+	}
+	.container {
+		height: calc(100vh - 74px);
+		width: 100%;
+		background: #018b7b;
+		display: grid;
+	}
+	.container-img {
+		width: 100%;
+		height: 80%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		object-position: top;
+		object-fit: cover;
+	}
+	.container-overlay {
+		width: 100%;
+		height: 100%;
+		position: fixed;
+		background-color: #fff;
+		filter: opacity(0.66);
+	}
+	h2 {
+		color: #000;
+		text-align: center;
+		font-size: 35px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+	.form-link span {
+		color: #273b4a;
+		font-size: 22px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+
+	.form-link {
+		text-align: center;
+		margin-top: 55px;
+	}
+	.signin-link {
+		color: #273b4a;
+		font-size: 22px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+		text-decoration-line: underline;
+	}
+	.form::-webkit-scrollbar {
+		display: none;
+	}
+
+	.form {
+		place-self: center;
+		max-width: 600px;
+		width: 100%;
+		max-height: 88vh;
+		height: 100%;
+		border-radius: 27px;
+		background: #fff;
+		padding: 0 50px 0 50px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+		overflow-y: scroll;
+		z-index: 1;
+	}
+
+	@media (max-width: 1280px) {
+		.form {
+			padding: 0 32px 0 32px;
+			max-width: 448px;
+		}
+		.form-link span,
+		.signin-link {
+			font-size: 14px;
+		}
+		.form-link {
+			margin-top: 22px;
+		}
+		h2 {
+			font-size: 26px;
+		}
+	}
+	@media (max-width: 1024px) {
+		.form {
+			max-width: 370px;
+			max-height: 85vh;
+		}
+		.form-link span,
+		.signin-link {
+			font-size: 11px;
+		}
+		.form-link {
+			margin-top: 14px;
+		}
+		h2 {
+			font-size: 22px;
+		}
+	}
+	@media (max-width: 768px) {
+		.note {
+			margin-top: 14px;
+		}
+		.form {
+			padding: 23px 28px 20px 28px;
+			max-width: 100%;
+			max-height: 100vh;
+			border-radius: 0;
+			justify-content: start;
+			z-index: 0;
+			overflow-y: auto;
+		}
+		.form-link span,
+		.signin-link {
+			font-size: 14px;
+		}
+		.form-link {
+			margin-top: 20px;
+		}
+		h2 {
+			color: #018b7b;
+			font-size: 25px;
+		}
+	}
+</style>
---
+++ src/routes/createNewPass/components/createNewPassForm/createNewPassForm.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+	import InputPassword from './inputPassword.svelte';
+</script>
+
+<form action="">
+	<div class="input-container">
+		<div class="radio-content">
+			<InputPassword
+				labelText="Enter a new password"
+				inputPlaceholder="New Password"
+				inputId="new-password"
+			/>
+			<InputPassword
+				labelText=""
+				inputPlaceholder="Repeat a new password"
+				inputId="new-password-repeat"
+			/>
+		</div>
+	</div>
+	<div class="field button-field">
+		<button class="next-step">Confirm</button>
+	</div>
+</form>
+
+<style>
+	.input-container {
+		width: 100%;
+		margin-bottom: 35px;
+		margin-top: 70px;
+	}
+	.radio-content {
+		padding: 0 10px;
+	}
+	.field .next-step {
+		width: 309px;
+		height: 65px;
+		flex-shrink: 0;
+		border-radius: 8px;
+		background: #00baa2;
+		background-color: rgb(0, 186, 162);
+		transition: all 0.3s ease;
+		cursor: pointer;
+		color: #273b4a;
+		text-align: center;
+		font-size: 20px;
+		font-style: normal;
+		font-weight: 600;
+		border: none;
+		outline: none;
+		white-space: nowrap;
+	}
+
+	.field .next-step:hover {
+		background-color: #00a790;
+	}
+	form {
+		width: 100%;
+	}
+	.button-field {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	@media (max-width: 1280px) {
+		.radio-content {
+			padding: 0;
+		}
+		.field .next-step {
+			width: 171px;
+			height: 52px;
+			font-size: 17px;
+		}
+		.input-container {
+			margin-bottom: 38px;
+			margin-top: 63px;
+		}
+	}
+	@media (max-width: 1024px) {
+		.input-container {
+			margin-bottom: 30px;
+			margin-top: 30px;
+		}
+		.field .next-step {
+			width: 218px;
+			height: 42px;
+			font-size: 14px;
+		}
+	}
+	@media (max-width: 768px) {
+		.input-container {
+			max-width: 100%;
+			margin-bottom: 50px;
+			margin-top: 50px;
+			width: 100%;
+		}
+		.radio-content {
+			margin-top: 0 !important;
+			padding: 0;
+		}
+		.field .next-step {
+			width: 317px;
+			height: 65px;
+			font-size: 22px;
+			white-space: nowrap;
+		}
+	}
+</style>

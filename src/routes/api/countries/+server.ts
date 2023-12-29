2756d347173d0eb132fe51f31ec21203f513a2ae
Seymur Bagirov <megadestroyer230@gmail.com>
2023-12-29 08:17:09 +0400

    return only countries in countres api(oops!)

--- src/routes/api/countries/+server.ts
+++ src/routes/api/countries/+server.ts
@@ -1,10 +1,12 @@
 import type { RequestHandler } from "./$types";
 import countriesJson from "../../../short-countries-cities.json";
 import type { Country } from "$lib/types";
+import { json } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async () => {
-    const countriesAndCities = countriesJson as Country[];
-    return new Response(JSON.stringify(countriesAndCities));
+    const countriesAndCities = countriesJson as {name: string, cities: string[]}[];
+    // this code is slow, need to optimize
+    return json(countriesAndCities.map(({name}) => ({name})))
 };
 
 

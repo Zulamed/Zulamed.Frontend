7a8836a52739c88f101201131cb94b062e910ab4
Seymur Bagirov <megadestroyer230@gmail.com>
2024-01-02 22:00:19 +0400

    improve registration page initial load speed

--- src/routes/api/countries/+server.ts
+++ src/routes/api/countries/+server.ts
@@ -1,12 +1,11 @@
 import type { RequestHandler } from "./$types";
-import countriesJson from "../../../short-countries-cities.json";
+import countriesJson from "../../../countries.json";
 import type { Country } from "$lib/types";
 import { json } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async () => {
-    const countriesAndCities = countriesJson as {name: string, cities: string[]}[];
-    // this code is slow, need to optimize
-    return json(countriesAndCities.map(({name}) => ({name})))
+    const countriesAndCities = countriesJson as {name: string}[];
+    return json(countriesAndCities);
 };
 
 
